52cb311adbb6923622dccf3ad4c7a20f329620e6
georgi5481 <georgi5481@abv.bg>
2021-06-03 18:01:53 +0300

    added messages

--- Mapper.cpp
+++ Mapper.cpp
@@ -1,8 +1,10 @@
 #include "IMapper.h"
 #include "Vec2.h"
 #include "IBorder.h"
+#include<iostream>
 
-class Mapper : IMapper<double> {
+class Mapper : IMapper<double> //using doubles for better accuracy for the coordinate system 
+{
 public:
 
 	Mapper() : velocity(1, 0) //to set the initial velocity 
@@ -27,7 +29,8 @@ public:
 
 
 	
-		if () {
+		if ()//we completed the map or there is some error 
+		{
 			return 0;
 		}
 	}
@@ -44,7 +47,15 @@ int main() {
 
 	Mapper obj;
 
-	obj.integrate();
+	double sensor(0);
+	for (unsigned int i = 0; obj.integrate( i, sensor) != 0 && std::cin.fail(); i++) {			//i is basically simulating a time and since there is no negative time - using unsigned int 
+		std::cin >> sensor;	//simulating a censor inputs
+	}
+
+
+
+
+	//obj.integrate();
 
 
 	
