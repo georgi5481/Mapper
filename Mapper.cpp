df2599279d2e1cdb7807bd3336288bcad28dc0ef
georgi5481 <georgi5481@abv.bg>
2021-06-06 14:55:16 +0300

    merged the two templates into this file

--- Mapper.cpp
+++ Mapper.cpp
@@ -1,8 +1,38 @@
 #include "IMapper.h"
 #include "Vec2.h"
 #include "IBorder.h"
-#include "Border.h"
+//#include "Border.h"
+
 #include<iostream>
+#include <list>
+template<class T>
+class Border : public IBorder<T>
+{
+public:
+	Border() {}
+
+	int checkIfLastBorder() {
+		if (polylines.size() > 1 && (polylines.begin()->x == polylines.rbegin()->x && polylines.begin()->y == polylines.rbegin()->y))
+		{			//if the container ins't empty and the pirst and last elements are the same in the coordinate system
+			return 0;
+		}
+		return 1;
+	}
+
+	void saveBorderCordinates(Vec2<T>& input) {
+		polylines.push_back(input);
+	}
+
+	virtual int getCount() const {
+		return polylines.size();
+	}
+	virtual const Vec2<T>& operator[](int index) const {
+		return *(std::next(polylines.begin(), index));
+	}
+private:
+	std::list<Vec2<T>> polylines;
+
+};
 
 template<class T>
 class Mapper : public IMapper<T> {
@@ -130,7 +160,7 @@ public:
 		position.y += (velocity.y * dt);
 
 		T ahead = *sensor;
-		T leftward = (*(sensor + 1));		//getting out the 4 values from the array
+		T leftward = (*(sensor + 1));		//getting out the 4 values from the input array
 		T astern = (*(sensor + 2));
 		T rightward = (*(sensor + 3));
 
@@ -163,11 +193,10 @@ private:
 	Vec2<T> position;	//the base constructor of Vec2 sets the X and Y cordinates to 0
 	Vec2<T> velocity;	//the base constructor of this class Mapper will set the initial velocity to (1, 0)
 	Vec2<T> right;	
-	Vec2<T> up;
+	Vec2<T> up;		//the four patterns we can go
 	Vec2<T> left;
 	Vec2<T> down;
-	//Vec2<T> pathVelocity[4] = { right, up, left, down };
-	Border<T> border;
+	Border<T> border;	//where we save the found borders
 };
 
 
